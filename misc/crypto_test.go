455492fdbb4ac646537c260fd9fb753b06ce6f8a
daniel robinson <daniel.robinson.open@gmail.com>
2017-01-04 18:51:26 +0000

    add ScryptKey test

--- misc/crypto_test.go
+++ misc/crypto_test.go
@@ -26,3 +26,18 @@ func Test_GenerateCryptoUrlSafeString(t *testing.T) {
 	assert.Equal(t, l, len(bs))
 	assert.Nil(t, err)
 }
+
+func Test_ScryptKey(t *testing.T) {
+	l := 4
+	pwd, _ := GenerateCryptoBytes(l)
+	salt, _ := GenerateCryptoBytes(l)
+	scryptPwd, err := ScryptKey(pwd, salt, l, l, l, l)
+	assert.Equal(t, l, len(scryptPwd))
+	assert.Nil(t, err)
+	l = 8
+	pwd, _ = GenerateCryptoBytes(l)
+	salt, _ = GenerateCryptoBytes(l)
+	scryptPwd, err = ScryptKey(pwd, salt, l, l, l, l)
+	assert.Equal(t, l, len(scryptPwd))
+	assert.Nil(t, err)
+}
