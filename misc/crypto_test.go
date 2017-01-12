c8837198a29b197b6798bb2a077d24ec6d8d44c5
daniel robinson <daniel.robinson.open@gmail.com>
2017-01-12 09:38:31 +0000

    update failing crypto tests

--- misc/crypto_test.go
+++ misc/crypto_test.go
@@ -5,39 +5,42 @@ import (
 	"testing"
 )
 
-func Test_GenerateCryptoBytes(t *testing.T) {
+func Test_Bytes(t *testing.T) {
 	l := 3
-	bs, err := CryptoBytes(l)
+	ch := NewCryptoHelper()
+	bs, err := ch.Bytes(l)
 	assert.Equal(t, l, len(bs))
 	assert.Nil(t, err)
 	l = 5
-	bs, err = CryptoBytes(l)
+	bs, err = ch.Bytes(l)
 	assert.Equal(t, l, len(bs))
 	assert.Nil(t, err)
 }
 
-func Test_GenerateCryptoUrlSafeString(t *testing.T) {
+func Test_UrlSafeString(t *testing.T) {
 	l := 3
-	bs, err := CryptoUrlSafeString(l)
+	ch := NewCryptoHelper()
+	bs, err := ch.UrlSafeString(l)
 	assert.Equal(t, l, len(bs))
 	assert.Nil(t, err)
 	l = 5
-	bs, err = CryptoUrlSafeString(l)
+	bs, err = ch.UrlSafeString(l)
 	assert.Equal(t, l, len(bs))
 	assert.Nil(t, err)
 }
 
 func Test_ScryptKey(t *testing.T) {
 	l := 4
-	pwd, _ := CryptoBytes(l)
-	salt, _ := CryptoBytes(l)
-	scryptPwd, err := ScryptKey(pwd, salt, l, l, l, l)
+	ch := NewCryptoHelper()
+	pwd, _ := ch.Bytes(l)
+	salt, _ := ch.Bytes(l)
+	scryptPwd, err := ch.ScryptKey(pwd, salt, l, l, l, l)
 	assert.Equal(t, l, len(scryptPwd))
 	assert.Nil(t, err)
 	l = 8
-	pwd, _ = CryptoBytes(l)
-	salt, _ = CryptoBytes(l)
-	scryptPwd, err = ScryptKey(pwd, salt, l, l, l, l)
+	pwd, _ = ch.Bytes(l)
+	salt, _ = ch.Bytes(l)
+	scryptPwd, err = ch.ScryptKey(pwd, salt, l, l, l, l)
 	assert.Equal(t, l, len(scryptPwd))
 	assert.Nil(t, err)
 }
