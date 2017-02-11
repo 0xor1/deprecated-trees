fdd014032d5750d9a4d5c782692f3e75946814b0
daniel robinson <daniel.robinson.open@gmail.com>
2017-02-11 15:53:22 +0000

    unit test internal wrapper

--- region_center/api/v1/internal/core_impl.go
+++ region_center/api/v1/internal/core_impl.go
@@ -34,56 +34,56 @@ func (a *api) CreatePersonalTaskCenter(region string, user Id) (int, error) {
 	if !a.IsValidRegion(region) {
 		return 0, invalidRegionErr
 	}
-	return 0, NotImplementedErr
+	return a.regions[region].createPersonalTaskCenter(user)
 }
 
 func (a *api) CreateOrgTaskCenter(region string, org, owner Id, ownerName string) (int, error) {
 	if !a.IsValidRegion(region) {
 		return 0, invalidRegionErr
 	}
-	return 0, NotImplementedErr
+	return a.regions[region].createOrgTaskCenter(org, owner, ownerName)
 }
 
 func (a *api) DeleteTaskCenter(region string, shard int, owner, account Id) (error, error) {
 	if !a.IsValidRegion(region) {
 		return nil, invalidRegionErr
 	}
-	return nil, NotImplementedErr
+	return a.regions[region].deleteTaskCenter(shard, owner, account)
 }
 
 func (a *api) AddMembers(region string, shard int, org, admin Id, members []*NamedEntity) (error, error) {
 	if !a.IsValidRegion(region) {
 		return nil, invalidRegionErr
 	}
-	return nil, NotImplementedErr
+	return a.regions[region].addMembers(shard, org, admin, members)
 }
 
 func (a *api) RemoveMembers(region string, shard int, org, admin Id, members []Id) (error, error) {
 	if !a.IsValidRegion(region) {
 		return nil, invalidRegionErr
 	}
-	return nil, NotImplementedErr
+	return a.regions[region].removeMembers(shard, org, admin, members)
 }
 
 func (a *api) SetMemberDeleted(region string, shard int, org, member Id) error {
 	if !a.IsValidRegion(region) {
 		return invalidRegionErr
 	}
-	return NotImplementedErr
+	return a.regions[region].setMemberDeleted(shard, org, member)
 }
 
 func (a *api) RenameMember(region string, shard int, org, member Id, newName string) error {
 	if !a.IsValidRegion(region) {
 		return invalidRegionErr
 	}
-	return NotImplementedErr
+	return a.regions[region].renameMember(shard, org, member, newName)
 }
 
 func (a *api) UserCanRenameOrg(region string, shard int, org, user Id) (bool, error) {
 	if !a.IsValidRegion(region) {
 		return false, invalidRegionErr
 	}
-	return false, NotImplementedErr
+	return a.regions[region].userCanRenameOrg(shard, org, user)
 }
 
 type internalApi interface {
--- region_center/api/v1/internal/core_impl_test.go
+++ region_center/api/v1/internal/core_impl_test.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	. "bitbucket.org/robsix/task_center/misc"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
@@ -30,6 +31,152 @@ func Test_newApi_success(t *testing.T) {
 
 	assert.NotNil(t, api)
 }
+
+func Test_apiIsValidRegion(t *testing.T) {
+	regions := map[string]internalApi{"us": &mockInternalApi{}}
+	api := newApi(regions, NewLog(nil))
+
+	assert.False(t, api.IsValidRegion("ch"))
+	assert.True(t, api.IsValidRegion("us"))
+}
+
+func Test_apiCreatePersonalTaskCenter(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	api := newApi(regions, NewLog(nil))
+
+	shard, err := api.CreatePersonalTaskCenter("ch", userId)
+	assert.Equal(t, 0, shard)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("createPersonalTaskCenter", userId).Return(2, testErr)
+	shard, err = api.CreatePersonalTaskCenter("us", userId)
+	assert.Equal(t, 2, shard)
+	assert.Equal(t, testErr, err)
+}
+
+func Test_apiCreateOrgTaskCenter(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	api := newApi(regions, NewLog(nil))
+
+	shard, err := api.CreateOrgTaskCenter("ch", orgId, userId, "ali")
+	assert.Equal(t, 0, shard)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("createOrgTaskCenter", orgId, userId, "ali").Return(2, testErr)
+	shard, err = api.CreateOrgTaskCenter("us", orgId, userId, "ali")
+	assert.Equal(t, 2, shard)
+	assert.Equal(t, testErr, err)
+}
+
+func Test_apiDeleteTaskCenter(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	api := newApi(regions, NewLog(nil))
+
+	publicErr, err := api.DeleteTaskCenter("ch", 2, userId, orgId)
+	assert.Nil(t, publicErr)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("deleteTaskCenter", 2, userId, orgId).Return(testErr, testErr2)
+	publicErr, err = api.DeleteTaskCenter("us", 2, userId, orgId)
+	assert.Equal(t, testErr, publicErr)
+	assert.Equal(t, testErr2, err)
+}
+
+func Test_apiAddMembers(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	members := make([]*NamedEntity, 2, 2)
+	api := newApi(regions, NewLog(nil))
+
+	publicErr, err := api.AddMembers("ch", 2, orgId, userId, members)
+	assert.Nil(t, publicErr)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("addMembers", 2, orgId, userId, members).Return(testErr, testErr2)
+	publicErr, err = api.AddMembers("us", 2, orgId, userId, members)
+	assert.Equal(t, testErr, publicErr)
+	assert.Equal(t, testErr2, err)
+}
+
+func Test_apiRemoveMembers(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	members := make([]Id, 2, 2)
+	api := newApi(regions, NewLog(nil))
+
+	publicErr, err := api.RemoveMembers("ch", 2, orgId, userId, members)
+	assert.Nil(t, publicErr)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("removeMembers", 2, orgId, userId, members).Return(testErr, testErr2)
+	publicErr, err = api.RemoveMembers("us", 2, orgId, userId, members)
+	assert.Equal(t, testErr, publicErr)
+	assert.Equal(t, testErr2, err)
+}
+
+func Test_apiSetMemberDeleted(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	api := newApi(regions, NewLog(nil))
+
+	err := api.SetMemberDeleted("ch", 2, orgId, userId)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("setMemberDeleted", 2, orgId, userId).Return(testErr)
+	err = api.SetMemberDeleted("us", 2, orgId, userId)
+	assert.Equal(t, testErr, err)
+}
+
+func Test_apiRenameMember(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	api := newApi(regions, NewLog(nil))
+
+	err := api.RenameMember("ch", 2, orgId, userId, "ali")
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("renameMember", 2, orgId, userId, "ali").Return(testErr)
+	err = api.RenameMember("us", 2, orgId, userId, "ali")
+	assert.Equal(t, testErr, err)
+}
+
+func Test_apiUserCanRenameOrg(t *testing.T) {
+	iApi := &mockInternalApi{}
+	regions := map[string]internalApi{"us": iApi}
+	userId, _ := NewId()
+	orgId, _ := NewId()
+	api := newApi(regions, NewLog(nil))
+
+	can, err := api.UserCanRenameOrg("ch", 2, orgId, userId)
+	assert.False(t, can)
+	assert.Equal(t, invalidRegionErr, err)
+
+	iApi.On("userCanRenameOrg", 2, orgId, userId).Return(true, testErr)
+	can, err = api.UserCanRenameOrg("us", 2, orgId, userId)
+	assert.True(t, can)
+	assert.Equal(t, testErr, err)
+}
+
+var (
+	testErr  = errors.New("test")
+	testErr2 = errors.New("test2")
+)
 
 type mockInternalApi struct {
 	mock.Mock
